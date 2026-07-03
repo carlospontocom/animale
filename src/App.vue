4202f6eed9ae3f403f3922a8d4232d6bca91661d
unknown <frontendcnn@gmail.com>
2026-07-03 18:42:36 -0300

    fix: adicionando novo paragráfo

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,8 @@
 
 <template>
   <h1>Bem-vindo</h1>
-  <p>
-   Sistema em criação do zero com Vue 3, Firebase e IA
-  </p>
+  <p>Sistema em criação do zero com Vue 3, Firebase e IA</p>
+  <p>Criando aplicações com vue de forma reaproveitável</p>
 </template>
 
 <style scoped></style>
