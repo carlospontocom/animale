d8337b51ac50d04b5e952eb1e1b22d04d4b31af6
unknown <frontendcnn@gmail.com>
2026-07-03 19:17:42 -0300

    Adicionando bloco de textos

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,7 @@
   <h1>Bem-vindo</h1>
   <p>Sistema em criação do zero com Vue 3, Firebase e IA</p>
   <p>Criando aplicações com vue de forma reaproveitável</p>
+  <p> Variáveis de ambiente configuradas melhor </p>
 </template>
 
 <style scoped></style>
